3762265c3b74a386c99c8dfb3fbcca62d671fec9
Carl Henrique <carlhenriquedev@gmail.com>
2025-10-14 06:19:39 -0300

    feat: funcoes de animacao header-scroll e menu hamburguer

---
+++ backend/scripts/index.js
@@ -0,0 +1,20 @@
+window.addEventListener("scroll", function () {
+
+    let header = document.querySelector("#header")
+    header.classList.toggle("rolagem", window.scrollY > 180)
+})
+
+
+function menuShow() {
+    const menuMobile = document.querySelector(".mobile-menu");
+    menuMobile.classList.toggle("open");
+
+    if (menuMobile.classList.contains("open")) {
+        const menuItems = menuMobile.querySelectorAll(".item-mobile");
+        menuItems.forEach(item => {
+            item.addEventListener("click", () => {
+                menuMobile.classList.remove("open");
+            });
+        });
+    }
+}
