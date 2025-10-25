fea3813d1c655122c1df141d83963ecd9450d748
Carl Henrique <carlhenriquedev@gmail.com>
2025-10-25 02:02:18 -0300

    docs: comentarios no index js

--- backend/scripts/index.js
+++ backend/scripts/index.js
@@ -1,10 +1,11 @@
+// === MUDAR ESTILO DA HEADER COM O SCROLL DA PÁGINA ===
 window.addEventListener("scroll", function () {
 
 let header = document.querySelector("#header")
 header.classList.toggle("rolagem", window.scrollY > 500)
 })
 
-
+// === MENU HAMBURGUER PARA TELAS MENORES ===
 function menuShow() {
 const menuMobile = document.querySelector(".mobile-menu");
 menuMobile.classList.toggle("open");
@@ -19,9 +20,9 @@ item.addEventListener("click", () => {
 }
 }
 
+// === ABRIR E FECHAR CARDS DO SECTION INFO (PROJETO, PARCERIA, BOLSISTAS) ===
 const cards = document.querySelectorAll('.info-card');
 const img = document.getElementById('info-img');
-
 cards.forEach(card => {
 card.addEventListener('click', () => {
 cards.forEach(c => c.classList.remove('active'));
@@ -30,6 +31,7 @@ img.src = `img/${card.id}.png`;
 });
 });
 
+// === ABRIR E FECHAR CARDS DOS BOLSISTAS ===
 const cards_ = document.querySelectorAll('.card');
 const modal = document.getElementById('modal-bolsista');
 const close = document.querySelector('.close');
@@ -39,7 +41,7 @@ const modalNome = document.getElementById('modal-nome');
 const modalCurso = document.getElementById('modal-curso');
 const modalDescricao = document.getElementById('modal-descricao');
 
-// Exemplo de descrições personalizadas (você pode ajustar)
+// dados de cada modal
 const infos = {
 "Andryene Rodrigues": {
 curso: "Administração",
@@ -48,7 +50,7 @@ descricao: "Descrição em breve."
 // ... adicione os outros nomes aqui
 };
 
-// Ao clicar em qualquer card
+
 cards_.forEach(card => {
 card.addEventListener('click', () => {
 const nome = card.querySelector('h4').textContent;
@@ -60,16 +62,17 @@ modalNome.textContent = nome;
 modalCurso.textContent = curso;
 modalDescricao.textContent = infos[nome]?.descricao || "Descrição em breve.";
 
+
 modal.style.display = 'flex';
 });
 });
 
-// Fecha o modal
+// Fecha o modal no icon x
 close.addEventListener('click', () => {
 modal.style.display = 'none';
 });
 
-// Fecha clicando fora
+// Fecha o modal clicando fora
 window.addEventListener('click', e => {
 if (e.target === modal) modal.style.display = 'none';
 });
